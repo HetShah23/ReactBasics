1f04377a6b3077f7691543ed7c696119ba76e4df
Het Shah <het2392000@gmail.com>
2020-10-02 12:27:58 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+import Album from './components/Album/Album';
+import PicContainer from './components/PicContainer/PicContainer';
+import Title from './components/Title';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  render() {
+    return (
+      <Router>
+        <Route exact path="/" component={Title} />
+        <Route exact path="/" component={Album} />
+        <Route path="/image/:id" component={PicContainer} />
+      </Router>
+    );
+  }
 }
 
 export default App;
